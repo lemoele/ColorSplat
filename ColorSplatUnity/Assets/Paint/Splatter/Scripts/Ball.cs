b905bf531f1afc2652724aa2e22c9f4b210bc67b
lemoele <elemoel@MacBook-Air-de-Emilie.local>
2015-11-23 18:25:37 +0100

    Associated particle system to ball throwing.
    DONE :
    In the ‘ThrowBall’ new prefab object :
    - Ball spawns at a random location when hitting space bar.
    - On contact with anything in the world, ball is destroyed.
    - Particle system is instantiated on contact point.
    - Particle system is destroyed after 12 seconds.
    
    TODO :
    - Randomize color.
    - Replace random location with event location on 4k screen.
    - Add ThrowBall object to main scene !!!

--- ColorSplatUnity/Assets/Paint/Splatter/Scripts/Ball.cs
+++ ColorSplatUnity/Assets/Paint/Splatter/Scripts/Ball.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
-	public GameObject splatterPrefab;
+	public GameObject splatter;
+	public GameObject particles;
+	private GameObject particlesClone;
 
 	void OnCollisionEnter(Collision collision) {
-		Vector3 new_pos = collision.contacts[0].point + new Vector3(0, 0, -0.1f);
-		Instantiate(splatterPrefab, new_pos, Quaternion.identity);
+//		Vector3 new_pos = collision.contacts[0].point + new Vector3(0, 0, -0.5f);
+//		Instantiate(splatter, new_pos, Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal));
+//		Instantiate(splatter, new_pos, Quaternion.identity);
+		particlesClone = Instantiate(particles, collision.contacts[0].point, Quaternion.identity) as GameObject;
+		Destroy(particlesClone, 12);
 		Destroy(this.gameObject);
+
 	}
 }
--- ColorSplatUnity/Assets/Paint/Splatter/Scripts/ThrowBall.cs
+++ ColorSplatUnity/Assets/Paint/Splatter/Scripts/ThrowBall.cs
@@ -13,9 +13,12 @@ public class ThrowBall : MonoBehaviour {
 		
 		if (Input.GetKeyDown("space"))
 		{
-			ballClone = Instantiate(ball, ballPos, transform.rotation) as GameObject;
-			Color randColor = new Color(Random.Range(0, 255)/255, Random.Range(0, 255)/255, Random.Range(0, 255)/255);
-			ballClone.GetComponent<MeshRenderer>().material.SetColor("_Albedo", randColor);
+			Vector3 rand = new Vector3(Random.Range(-10, 10), Random.Range(-8, 8), 0);
+			ballClone = Instantiate(ball, ballPos + rand, transform.rotation) as GameObject;
+
+//			Color randColor = new Color(Random.Range(0, 255)/255, Random.Range(0, 255)/255, Random.Range(0, 255)/255);
+//			ballClone.GetComponent<MeshRenderer>().material.SetColor("_Albedo", randColor);
+
 			ballClone.GetComponent<Rigidbody>().AddForce(throwSpeed, ForceMode.Impulse);
 		}
 	}
