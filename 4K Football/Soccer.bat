f6b0b35599026d89f375e5093a5ede36e4f34ed2
lemoele <elemoel@n184-p100.kthopen.kth.se>
2015-11-25 10:59:48 +0100

    Delete 4K football useless files from the git.

--- 4K Football/Soccer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start C:\SoccerDemo\CCV.lnk
-echo Remember to calibrate CCV
-pause
-echo Starting local server on port 80...
-start "Soccer Demo" "http://localhost:80/"
-node "C:\SoccerDemo\server.js"
