1b984131a2be880cfba91750e52a5a37315dbead
lemoele <elemoel@MacBook-Air-de-Emilie.local>
2015-11-28 11:34:47 +0100

    First try at a TUIO input -- not working YET.
    - Imported TUIO C# .NET plugin.
    - Adapted the plugin to hide non-compiling parts in Unity.
    - Added a script TuioTest (based on the example provided) to get the
    ball position from the Tuio input.
    - Changed the ThrowBall prefab to attach this script and link it to
    Throw script.
    
    Not working :
    - May be because of refresh method not overloaded correctly.
    - May be because of the TUIO client not linking correctly to CCV.
    - May be because of the foreach loop on cursors (would be better to
    take just one, the first cursor that has been included — maybe use
    something different from Dictionaries ?)

---
+++ ColorSplatUnity/Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowBall : MonoBehaviour {
+
+	public Camera cam;
+	public GameObject ball;
+	public Vector3 throwSpeed; 
+	private Vector3 ballPos; 
+	private GameObject ballClone; 
+	private float X, Y;
+
+	void Start () {
+		X = Y = -1.0f;
+	}
+
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane);
+			launchBall(mousePos);
+		}
+
+		if (X > 0 && Y > 0) {
+			Vector3 tuioPos = new Vector3(X, Y, cam.nearClipPlane);
+			launchBall(tuioPos);
+		}
+	}
+
+	public float tuioX {
+		get {return X;}
+		set {X = value;}
+	}
+	
+	public float tuioY {
+		get {return Y;}
+		set {Y = value;}
+	}
+
+	void launchBall (Vector3 pos) {
+		ballPos = cam.ScreenToWorldPoint(pos) + new Vector3(0, 0, 0.1f);
+		ballClone = Instantiate(ball, ballPos, transform.rotation) as GameObject;
+		//			Color randColor = new Color(Random.Range(0, 255)/255, Random.Range(0, 255)/255, Random.Range(0, 255)/255);
+		//			ballClone.GetComponent<MeshRenderer>().material.SetColor("_Albedo", randColor);
+		ballClone.GetComponent<Rigidbody>().AddForce(throwSpeed, ForceMode.Impulse);
+
+	}
+}
---
+++ ColorSplatUnity/Assets/TUIO11_NET/TuioTest.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using TUIO;
+
+public class TuioTest : MonoBehaviour, TuioListener {
+	
+	private TuioClient client;
+	private Dictionary<long,TuioObject> objectList;
+	private Dictionary<long,TuioCursor> cursorList;
+	private Dictionary<long,TuioBlob> blobList;
+	private object cursorSync = new object();
+	private object objectSync = new object();
+	private object blobSync = new object();
+	private int port = 3333;
+
+	private bool fullscreen;
+	private bool verbose;
+	
+	private float Xpos;
+	private float Ypos;
+
+	// Use this for initialization
+	void Start () {
+		verbose = true;
+		fullscreen = false;
+
+		objectList = new Dictionary<long,TuioObject>(128);
+		cursorList = new Dictionary<long,TuioCursor>(128);
+		blobList = new Dictionary<long,TuioBlob>(128);
+		
+		client = new TuioClient(port);
+		client.addTuioListener(this);
+		client.connect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cursorList.Count > 0) {
+			lock(cursorSync) {
+				foreach (TuioCursor tcursor in cursorList.Values) {
+					Xpos = tcursor.X;
+					Ypos = tcursor.Y;
+				}
+			}
+		} else {
+			Xpos = Ypos = -1.0f;
+		}
+		this.GetComponent<ThrowBall>().tuioX = Xpos;
+		this.GetComponent<ThrowBall>().tuioY = Ypos;
+		cursorList.Clear();
+	}
+
+	public void addTuioObject(TuioObject o) {
+		lock(objectSync) {
+			objectList.Add(o.SessionID,new TuioObject(o));
+		} if (verbose) Console.WriteLine("add obj "+o.SymbolID+" ("+o.SessionID+") "+o.X+" "+o.Y+" "+o.Angle);
+	}
+	
+	public void updateTuioObject(TuioObject o) {
+		lock(objectSync) {
+			objectList[o.SessionID].update(o);
+		}
+		if (verbose) Console.WriteLine("set obj "+o.SymbolID+" "+o.SessionID+" "+o.X+" "+o.Y+" "+o.Angle+" "+o.MotionSpeed+" "+o.RotationSpeed+" "+o.MotionAccel+" "+o.RotationAccel);
+	}
+	
+	public void removeTuioObject(TuioObject o) {
+		lock(objectSync) {
+			objectList.Remove(o.SessionID);
+		}
+		if (verbose) Console.WriteLine("del obj "+o.SymbolID+" ("+o.SessionID+")");
+	}
+	
+	public void addTuioCursor(TuioCursor c) {
+		lock(cursorSync) {
+			cursorList.Add(c.SessionID,c);
+		}
+		if (verbose) Console.WriteLine("add cur "+c.CursorID + " ("+c.SessionID+") "+c.X+" "+c.Y);
+	}
+	
+	public void updateTuioCursor(TuioCursor c) {
+		if (verbose) Console.WriteLine("set cur "+c.CursorID + " ("+c.SessionID+") "+c.X+" "+c.Y+" "+c.MotionSpeed+" "+c.MotionAccel);
+	}
+	
+	public void removeTuioCursor(TuioCursor c) {
+		lock(cursorSync) {
+			cursorList.Remove(c.SessionID);
+		}
+		if (verbose) Console.WriteLine("del cur "+c.CursorID + " ("+c.SessionID+")");
+	}
+
+	public void addTuioBlob(TuioBlob b) {
+		lock(blobSync) {
+			blobList.Add(b.SessionID,b);
+		}
+		if (verbose) Console.WriteLine("add blb "+b.BlobID + " ("+b.SessionID+") "+b.X+" "+b.Y+" "+b.Angle+" "+b.Width+" "+b.Height+" "+b.Area);
+	}
+	
+	public void updateTuioBlob(TuioBlob b) {
+		if (verbose) Console.WriteLine("set blb "+b.BlobID + " ("+b.SessionID+") "+b.X+" "+b.Y+" "+b.Angle+" "+b.Width+" "+b.Height+" "+b.Area+" "+b.MotionSpeed+" "+b.RotationSpeed+" "+b.MotionAccel+" "+b.RotationAccel);
+	}
+	
+	public void removeTuioBlob(TuioBlob b) {
+		lock(blobSync) {
+			blobList.Remove(b.SessionID);
+		}
+		if (verbose) Console.WriteLine("del blb "+b.BlobID + " ("+b.SessionID+")");
+	}
+
+	public void refresh(TuioTime frameTime) {
+//		Invalidate();
+	}
+
+}
